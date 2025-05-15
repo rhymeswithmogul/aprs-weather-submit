b514e3622f0792503e5fb472d47b6173bc5812ac
Colin Cogle <colin@colincogle.name>
2025-05-15 19:42:40 -0400

    Bump to version 2.0.
    This is the biggest version bump commit ever, as you can tell by
    the increase in the major version.
    
    This commit updates all documentation, configuration files, and
    distribution-specific files.

--- src/main.h
+++ src/main.h
@@ -28,7 +28,7 @@ with this program.  If not, see <https://www.gnu.org/licenses/agpl-3.0.html>.
 #endif
 
 #ifndef VERSION
-#define VERSION "1.9-dev"
+#define VERSION "2.0"
 #endif
 
 /* Official builds of aprs-weather-submit may use the assigned TOCALL
